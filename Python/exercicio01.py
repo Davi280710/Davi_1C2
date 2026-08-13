215d9f30552804bffb3cbd387f29c9a92241c025
Davi Rodrigues Alves <davirodriguesalves087@gmail.com>
2026-08-13 13:24:50 -0300

    Create exercicio01.py

---
+++ Python/exercicio01.py
@@ -0,0 +1,15 @@
+autores: Davi Rodrigues Alves e Iago Fernando
+Etec vasco antonio venchirutte
+Enuciado: Leia um número inteiro e informe se ele é positivo, negativo ou igual a zero.
+
+
+numero = int(input("digite um numero:"))
+
+if numero > 0:
+  print("seu numero é psitivo")
+
+elif numero < 0:
+  print("seu numero é negativo")
+  
+else:
+  print("seu numero é 0")
